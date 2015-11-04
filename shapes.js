ee11583afc8416125e76289f17a46d2b9cae58ad
Thomas Russel Carrel <annon>
2015-11-04 14:59:31 -0800

    Fixed loop-counters for procedural generation of plane.  get_plane()

--- shapes.js
+++ shapes.js
@@ -32,47 +32,53 @@ function get_skybox()
 //
 function get_plane(x_steps, z_steps, x_len, z_len)
 {
-
+    debugger;
 
     var dx = x_len / x_steps;
     var dz = z_len / z_steps;
+
+    var top_left     = [0.0, 0.0];
+    var top_right    = [ dx, 0.0];
+    var bottom_left  = [0.0,  dz];
+    var bottom_right = [ dx,  dz];
 
     var plane = [];
     var z_adg = Math.floor( z_steps / 2);
     var x_adg = Math.floor( z_steps / 2);
 
-    debugger;
-
     var direction = 1;
-    for( var z1 = -z_adg, z2 = 1 - z_adg; z2 < z_adg + z_len; z1 += dz, z2 += dz )
+    for( var z = -z_adg; z < z_adg; z++ )
     {
         // Increment loop if direction is positive,
         //decrement loop if direction is negative.
         for( 
                 var x = ((direction > 0) ? 0 : x_adg - 1);
-                ((direction > 0) ? (x < x_adg + x_len) : (x >= -x_adg));
-                x += direction * dx;
+                ((direction > 0) ? (x < x_adg) : (x >= -x_adg));
+                x += direction
            )
         {
             //Vertex coordinate 1.
-            plane.push(x * dx);
+            plane.push(x_adg + (x * top_left[0]));
             plane.push(0);
-            plane.push(z1 * dz);
+            plane.push(z_adg + (z * top_left[1]));
 
             //Texture coordinate 1.
             plane.push(x * dx);
-            plane.push(z1 * dz);
+            plane.push(z * dz);
 
+            /*
             //Vertex coordinate 2;
             plane.push(x * dx);
             plane.push(0);
-            plane.push(z2 * dz);
+            plane.push(z * dz);
 
             //Texture coordinate 2;
             plane.push(x * dx);
             plane.push(z2 * dz);
             direction = direction * -1;
+            */
         }
+        direction = -direction;
     }
 
     return new Float32Array(plane);
